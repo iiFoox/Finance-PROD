77f1f984a39d1cee09084b14c468bb4760debd31
iiFoox <vfbnn29@gmail.com>
2025-07-17 09:16:46 -0300

    fix: wrap public routes with PublicRoute component for Vercel compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -79,16 +79,14 @@ const AppContent: React.FC = () => {
       <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
       <Route path="/forgot-password" element={<PublicRoute><ForgotPasswordPage /></PublicRoute>} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
-      
-      {/* Páginas Públicas - Removido PublicRoute para funcionar no Vercel */}
-      <Route path="/recursos" element={<RecursosPage />} />
-      <Route path="/precos" element={<PrecosPage />} />
-      <Route path="/contato" element={<ContatoPage />} />
-      <Route path="/sobre" element={<SobrePage />} />
-      <Route path="/integracoes" element={<IntegracoesPage />} />
-      <Route path="/ajuda" element={<AjudaPage />} />
-      <Route path="/documentacao" element={<DocumentacaoPage />} />
-      <Route path="/status" element={<StatusPage />} />
+      <Route path="/recursos" element={<PublicRoute><RecursosPage /></PublicRoute>} />
+      <Route path="/precos" element={<PublicRoute><PrecosPage /></PublicRoute>} />
+      <Route path="/contato" element={<PublicRoute><ContatoPage /></PublicRoute>} />
+      <Route path="/sobre" element={<PublicRoute><SobrePage /></PublicRoute>} />
+      <Route path="/integracoes" element={<PublicRoute><IntegracoesPage /></PublicRoute>} />
+      <Route path="/ajuda" element={<PublicRoute><AjudaPage /></PublicRoute>} />
+      <Route path="/documentacao" element={<PublicRoute><DocumentacaoPage /></PublicRoute>} />
+      <Route path="/status" element={<PublicRoute><StatusPage /></PublicRoute>} />
       
       {/* Rotas Privadas */}
       <Route path="/dashboard" element={<PrivateRoute><Layout><DashboardPage /></Layout></PrivateRoute>} />
